75dcd7124c2de96c46e9ad093ff776b7b99d0d65
Uģis Ozols <ugis.ozolss@gmail.com>
2012-03-02 21:10:10 +0200

    There's no seeds file so we don't need this code.

--- lib/generators/refinery/settings_generator.rb
+++ lib/generators/refinery/settings_generator.rb
@@ -5,14 +5,5 @@ module Refinery
       rake("refinery_settings:install:migrations")
     end
 
-    def append_load_seed_data
-      create_file 'db/seeds.rb' unless File.exists?(File.join(destination_root, 'db', 'seeds.rb'))
-      append_file 'db/seeds.rb', :verbose => true do
-        <<-EOH
-
-Refinery::Settings::Engine.load_seed
-        EOH
-      end
-    end
   end
 end
