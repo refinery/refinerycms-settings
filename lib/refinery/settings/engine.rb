ccc437d4e9d4edf69fb8ab74926a5567ec6f93cf
Philip Arndt <parndt@gmail.com>
2012-03-06 12:50:38 +1300

    Updated to support Refinery CMS 2.0.1

--- lib/refinery/settings/engine.rb
+++ lib/refinery/settings/engine.rb
@@ -14,7 +14,7 @@ module Refinery
           plugin.name = 'refinery_settings'
           plugin.version = %q{2.0.0}
           plugin.menu_match = %r{refinery/settings$}
-          plugin.url = { :controller => '/refinery/admin/settings' }
+          plugin.url = proc { Refinery::Core::Engine.routes.url_helpers.admin_settings_path }
         end
       end
 
